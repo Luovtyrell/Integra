6b421f462ca233f608b5856155c56f280598db31
Luovtyrell <luciakjhgfds@gmail.com>
2024-10-22 17:16:03 +0200

    Implemented Charts.js to display monthly precipitations

---
+++ src/components/charts/PrecipitationCharts.jsx
@@ -0,0 +1,54 @@
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const PrecipitationChart = () => {
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September'],
+    datasets: [
+      {
+        label: 'Precipitacions (mm)',
+        data: [1.7, 90.3, 71.0, 4.1, 49.8, 20.8, 10.6, 107.9, 63.9],
+        borderColor: 'rgba(75, 192, 192, 1)',
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        pointBackgroundColor: 'rgba(54, 162, 235, 1)', 
+        fill: true,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Precipitacions del 1990',
+      },
+    },
+  };
+
+  return <Line data={data} options={options} />;
+};
+
+export default PrecipitationChart;
--- src/pages/RainPage/RainPage.jsx
+++ src/pages/RainPage/RainPage.jsx
@@ -1,10 +1,15 @@
 import NavbarHeader from "../../components/navbars/NavbarHeader";
 import NavbarFooter from "../../components/navbars/NavbarFooter";
+import PrecipitationChart from "../../components/charts/PrecipitationCharts";
 
 function RainPage() {
   return (
     <>
       <NavbarHeader />
+      <div style={{ padding: "20px", textAlign: "center" }}>
+        <h1>Precipitacions mensuals</h1>
+        <PrecipitationChart />
+      </div>
       <NavbarFooter />
     </>
   );
